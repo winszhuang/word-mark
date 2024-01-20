b418f624d11e19f0f4b1295e5fa16191ee9418db
wins <win4709hi@gmail.com>
2024-01-20 17:11:27 +0800

    重構 抽取tooltip邏輯至useTooltip

---
+++ frontend/src/composables/use-tooltip.ts
@@ -0,0 +1,71 @@
+import { ref } from 'vue'
+import { extractSentenceFromClick } from '../utils/sentence'
+import { translate } from '../utils/google-translate'
+import { useWords } from './use-words'
+import { buffer, debounceTime, filter, fromEvent, map } from 'rxjs'
+import { ROOT_ID } from '../content-script/constants/constants'
+import Swal from 'sweetalert2'
+
+export function useTooltip() {
+  const { wordHandler } = useWords()
+  const word = ref<Word>({ text: '', sentence: '', explains: [] })
+  const showTooltip = ref(false)
+  const tooltipPosition = ref({ left: 0, top: 0 })
+  const click$ = fromEvent(document, 'click')
+  const clickOutsideTooltip$ = click$.pipe(
+    filter((e) => {
+      const el = e.target as HTMLElement
+      const isTooltipEl = el.closest(`#${ROOT_ID}`)
+      return !isTooltipEl
+    })
+  )
+
+  clickOutsideTooltip$.subscribe(() => {
+    showTooltip.value = false
+  })
+
+  const doubleClick$ = click$.pipe(
+    buffer(click$.pipe(debounceTime(250))),
+    filter((clickArray) => clickArray.length === 2),
+    map((clickArray) => {
+      const selectedText = window.getSelection()?.toString() || ''
+      const cleanText = selectedText.replace(/[^a-zA-Z\s]/g, '')
+      return {
+        text: cleanText.trim(),
+        event: clickArray[1] // 第二次點擊的事件
+      }
+    })
+  )
+
+  doubleClick$.subscribe(async ({ text, event }) => {
+    const result = await translate({ text: text! })
+    if (!result) {
+      Swal.fire({
+        title: 'Error',
+        text: 'fail to get translate data!!',
+        icon: 'error',
+        confirmButtonText: 'confirm'
+      })
+      return
+    }
+    word.value = {
+      text: result.text,
+      sentence: extractSentenceFromClick(event.target as HTMLElement) || '',
+      explains: result.detailed ? result.detailed : result.result || []
+    }
+    const e = event as PointerEvent
+    tooltipPosition.value = { left: e.clientX, top: e.clientY + window.scrollY }
+    showTooltip.value = true
+  })
+
+  const onSave = (word: Word) => {
+    wordHandler.add(word)
+  }
+
+  return {
+    tooltipPosition,
+    showTooltip,
+    word,
+    onSave
+  }
+}
--- frontend/src/content-script/App.vue
+++ frontend/src/content-script/App.vue
@@ -1,67 +1,8 @@
 <script setup lang="ts">
-import { buffer, debounceTime, filter, fromEvent, map } from 'rxjs'
-import { ref } from 'vue'
-import { translate } from '../utils/google-translate'
-import { extractSentenceFromClick } from '../utils/sentence'
-import { ROOT_ID } from './constants/constants'
-import Swal from 'sweetalert2'
 import Tooltip from './components/Tooltip.vue'
-import { useWords } from '../composables/use-words'
+import { useTooltip } from '../composables/use-tooltip'
 
-const { wordHandler } = useWords()
-const word = ref<Word>({ text: '', sentence: '', explains: [] })
-const showTooltip = ref(false)
-const tooltipPosition = ref({ left: 0, top: 0 })
-const click$ = fromEvent(document, 'click')
-const clickOutsideTooltip$ = click$.pipe(
-  filter((e) => {
-    const el = e.target as HTMLElement
-    const isTooltipEl = el.closest(`#${ROOT_ID}`)
-    return !isTooltipEl
-  })
-)
-
-clickOutsideTooltip$.subscribe(() => {
-  showTooltip.value = false
-})
-
-const doubleClick$ = click$.pipe(
-  buffer(click$.pipe(debounceTime(250))),
-  filter((clickArray) => clickArray.length === 2),
-  map((clickArray) => {
-    const selectedText = window.getSelection()?.toString() || ''
-    const cleanText = selectedText.replace(/[^a-zA-Z\s]/g, '')
-    return {
-      text: cleanText.trim(),
-      event: clickArray[1] // 第二次點擊的事件
-    }
-  })
-)
-
-doubleClick$.subscribe(async ({ text, event }) => {
-  const result = await translate({ text: text! })
-  if (!result) {
-    Swal.fire({
-      title: 'Error',
-      text: 'fail to get translate data!!',
-      icon: 'error',
-      confirmButtonText: 'confirm'
-    })
-    return
-  }
-  word.value = {
-    text: result.text,
-    sentence: extractSentenceFromClick(event.target as HTMLElement) || '',
-    explains: result.detailed ? result.detailed : result.result || []
-  }
-  const e = event as PointerEvent
-  tooltipPosition.value = { left: e.clientX, top: e.clientY + window.scrollY }
-  showTooltip.value = true
-})
-
-const onSave = (word: Word) => {
-  wordHandler.add(word)
-}
+const { tooltipPosition, showTooltip, word, onSave } = useTooltip()
 </script>
 
 <template>
@@ -73,18 +14,3 @@ const onSave = (word: Word) => {
     @save="onSave"
   />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
